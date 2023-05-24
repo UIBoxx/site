d7f4fa5bf8e2d133209c33066d81631c1be0428c
Prabin Bhatt <132075349+UIBoxx@users.noreply.github.com>
2023-05-24 15:57:34 +0545

    commit

--- src/tools/GradientBackgroundGenerator.tsx
+++ src/tools/GradientBackgroundGenerator.tsx
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import "../CSS/tutorial.css";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { zIndex } from "html2canvas/dist/types/css/property-descriptors/z-index";
 
 function GradientBackgroundGenerator() {
   const [gradientProperties, setGradientProperties] = useState({
