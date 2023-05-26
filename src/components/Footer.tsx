2af0b03c346aeec8fb3ec657b867fdd97f059153
Prabin Bhatt <132075349+UIBoxx@users.noreply.github.com>
2023-05-26 12:35:51 +0545

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,22 +5,33 @@ function Footer() {
     <footer>
       <div className="address-container">
         <div className="address">
-          <a href="/tutorials">
-            <h4>Tutorials</h4>
-          </a>
-          <a href="">
-            <h4>Policy</h4>
-          </a>
-          <a href="/UI-Library">
-            <h4>Free UI Components</h4>
-          </a>
-          <a href="/tools">
-            <h4>Tools</h4>
-          </a>
-
-          <a href="/contact">
-            <h4>Contact Us</h4>
-          </a>
+          <ul className="beautiful-bullets">
+            <li>
+              <a href="/tutorials">
+                <h4>Tutorials</h4>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <h4>Policy</h4>
+              </a>
+            </li>
+            <li>
+              <a href="/UI-Library">
+                <h4>Free UI Components</h4>
+              </a>
+            </li>
+            <li>
+              <a href="/tools">
+                <h4>Tools</h4>
+              </a>
+            </li>
+            <li>
+              <a href="/contact">
+                <h4>Contact Us</h4>
+              </a>
+            </li>
+          </ul>
         </div>
         <div className="address">
           <h5>Kathmandu, Nepal</h5>
@@ -28,7 +39,9 @@ function Footer() {
           <h5>contact@uiboxx.in</h5>
         </div>
       </div>
-      <p><span>UIBoxx </span>2023, all rights reserved <br /></p>
+      <p>
+        <span>UIBoxx </span>2023, all rights reserved <br />
+      </p>
     </footer>
   );
 }
